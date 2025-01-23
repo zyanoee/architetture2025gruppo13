96b560e9198a0798e1464e4004a23d2731393bd2
Matteo Pagano <matteopagano16@gmail.com>
2025-01-23 11:54:36 +0100

    Test di backbone e distance in 64

--- src/pst64.nasm
+++ src/pst64.nasm
@@ -120,3 +120,294 @@ prova:
 		mov		rsp, rbp	; ripristina lo Stack Pointer
 		pop		rbp		    ; ripristina il Base Pointer
 		ret				    ; torna alla funzione C chiamante
+
+
+global backbone_asm
+align 32
+backbone_asm:
+
+    push		ebp		; salva il Base Pointer
+    mov		ebp, esp	; il Base Pointer punta al Record di Attivazione corrente
+    push		ebx		; salva i registri da preservare
+    push		esi
+    push		edi
+
+    movss   ymm0, [dist0]         ; dist0 = 1.46
+    movss   ymm1, [dist1]         ; dist1 = 1.52
+    movss   ymm2, [dist2]         ; dist2 = 1.33
+    movss   ymm3, [angle_cnca]    ; angle_cnca = 2.124
+
+    mov ebx, [ebp + 8]
+
+    ; Alloca memoria per coords
+    push    ebx
+    imul    ebx, 3
+
+
+    push    ebx
+    push    4                   
+    call    alloc_matrix
+    add     esp, 8
+    mov     esi, eax            
+
+    push 4
+    call alloc_matrix
+    add esp, 4
+    mov ecx, eax
+    pop ebx
+
+    ; Imposta i primi valori di coords
+    xorps   ymm4, ymm4
+    vmovapd  [esi], ymm4         ; coords[0] = 0, coords[1] = 0, coords[2] = 0, coords[3] = 0 (Padding)
+	addps  ymm4, ymm0          ; xmm4 = dist0
+    vmovapd  [esi + 32], ymm4    ; coords[4] = dist0, coords[5] = 0, coords[6] = 0, coords[7] = 0 (Padding)
+	
+
+	
+    ; Prepara il ciclo per i calcoli
+    xor     edi, edi            ; i = 0
+
+.loop:
+    ; Calcola idx
+    mov     edx, edi
+    imul    edx, 12      ; idx = i * 12
+
+    ; Salta il calcolo di N e CA per la prima iterazione
+    cmp     edi, 0
+    je      .skip_c
+
+    ; Calcola N (tmp[0..3] = coords[4*(idx-1)] - coords[4*(idx-2)])
+
+    vmovapd  ymm5, [esi + edx*8 - 32]
+    vmovapd  ymm6, [esi + edx*8 - 64]
+    vsubpd    ymm5, ymm6
+    vmovapd  [ecx], ymm5         ; tmp = coords[4*(idx-1)] - coords[4*(idx-2)]
+
+
+
+    push ecx
+
+    ; Ruota tmp con angle_cnca
+
+    vextractf128 xmm1, ymm3, 0x0
+    movd eax, xmm1
+    push eax
+    push ecx
+    call rotation
+    add esp, 8
+    pop ecx
+
+
+    push eax
+
+    ; Crea nuovo vettore (tmp e dist2)
+    vxorpd   ymm5, ymm5
+
+    vextractf128 xmm7, ymm2, 0x0        
+    movsd xmm7, xmm7                   
+    vshufpd xmm7, xmm7, xmm7, 0x1      
+    vblendpd ymm5, ymm5, xmm7, 0b0010  
+
+    vmovapd  [ecx], xmm5
+
+    pop eax
+    push ecx
+
+
+
+    push eax
+    push ecx
+    call matrix_product
+    add esp, 8
+
+    pop ecx
+    push eax
+
+
+    mov     edx, edi
+    imul    edx, 12
+    pop eax
+    vmovapd ymm5, [eax]
+    vmovapd ymm4, [esi + edx*8 - 32]
+    vaddpd ymm4, ymm5
+    vmovapd [esi + edx*8], ymm4
+
+
+
+    ; Calcola CA (tmp[0..3] = coords[4*(idx+1)] - coords[4*(idx)])
+    vmovapd  ymm5, [esi + edx*8]
+    vmovapd  ymm6, [esi + edx*8 - 32]
+    vsubpd    ymm5, ymm6
+    vmovapd  [ecx], ymm5
+
+
+    ; Ruota tmp con phi[i]
+
+    mov eax, [ebp+12]
+    mov eax, [eax+edi*8]
+
+    push ecx
+
+    push    eax
+    push    ecx     ; phi[i]
+    call    rotation
+    add     esp, 8
+
+
+    pop ecx
+    push eax
+
+    ; Crea nuovo vettore (tmp e dist0)
+    vxorpd ymm5, ymm5
+
+    vextractf128 xmm7, ymm0, 0x0        
+    movsd xmm7, xmm7                   
+    vshufpd xmm7, xmm7, xmm7, 0x1      
+    vblendpd ymm5, ymm5, xmm7, 0b0010  
+
+    vmovapd  [ecx], ymm5
+
+
+    pop eax
+    push ecx
+
+    push eax
+    push ecx
+    call matrix_product
+    add esp,8
+
+    pop ecx
+
+    push eax
+
+    mov     edx, edi
+    imul    edx, 12
+
+    pop eax
+
+    vmovapd ymm5, [eax]
+    vmovapd ymm4, [esi + edx*8]
+    vaddpd ymm5, ymm4
+
+    vmovapd  [esi + edx*8 + 32], ymm5 ; Assegna risultato in coords[idx+1]
+
+.skip_c:
+
+    ; Calcola C (tmp[0..3] = coords[4*(idx+2)] - coords[4*(idx+1)])
+    vmovapd  ymm5, [esi + edx*8 + 32]
+    vmovapd  ymm6, [esi + edx*8]
+    vsubpd    ymm5, ymm6
+    vmovapd  [ecx], ymm5
+
+    ; Ruota tmp con psi[i]
+
+    mov eax, [ebp+16]
+    mov eax, [eax+edi*8]
+
+
+    push ecx
+
+    push    eax
+    push    ecx       ; psi[i]
+    call    rotation
+    add     esp, 8
+
+
+    pop ecx
+
+
+    ; Crea nuovo vettore (tmp e dist0)
+    vxorpd ymm5, ymm5
+
+    vextractf128 xmm7, ymm1, 0x0        
+    movsd xmm7, xmm7                   
+    vshufpd xmm7, xmm7, xmm7, 0x1      
+    vblendpd ymm5, ymm5, xmm7, 0b0010  
+
+    vmovapd  [ecx], ymm5
+
+
+
+    push ecx
+
+    push eax
+    push ecx
+    call matrix_product
+    add esp,8
+
+    pop ecx
+
+    mov     edx, edi
+    imul    edx, 12
+
+    vmovapd ymm5, [eax]
+    vmovapd ymm4, [esi + edx*8 + 32]
+    vaddpd ymm5, ymm4
+
+    vmovapd  [esi + edx*8 + 64], ymm5 ; Assegna risultato in coords[idx+1]
+
+    ; Incrementa i registri e continua il ciclo
+    add     edi, 1
+    cmp     edi, ebx
+    jl      .loop
+
+    ; Fine ciclo
+    ; Dealloca memoria e ripristina i registri
+    fremem ecx
+    mov eax, esi
+    pop     edi
+    pop     esi
+    pop     ebx
+    mov     esp, ebp
+    pop     ebp
+    ret
+
+
+global distance_asm
+align 32
+distance_asm:
+    push		ebp		; salva il Base Pointer
+    mov		ebp, esp	; il Base Pointer punta al Record di Attivazione corrente
+    push		ebx		; salva i registri da preservare
+    push		esi
+    push		edi
+
+    mov edx, [ebp + 8]        ; edx = i
+    mov edi, [ebp + 12]       ; edi = j
+
+    shl edx, 2                ; edx = i * 4
+    shl edi, 2                ; edi = j * 4 
+
+    mov esi, [ebp + 16]       ; esi = c_alpha_coords
+
+
+
+    vmovapd ymm0, [esi+edx*8] ; coords[i]
+    vmovapd ymm1, [esi+edi*8] ; coord[j]
+    vsubpd ymm1, ymm0         ; xmm1 = coords[j] - coords[i]
+
+
+    vmulpd ymm1, ymm1
+
+
+    haddpd ymm1, ymm1         ; somma parziale
+    haddpd ymm1, ymm1         ; somma totale
+
+
+    sqrtsd ymm1, ymm1         ; sqrt(xmm1)
+    vextractf128 xmm1, ymm1, 0   ; Estrae la parte inferiore di ymm1 (128 bit) in xmm1
+    movd eax, xmm1               ; Sposta il valore di xmm1 (double) in eax
+
+    push eax
+    fld dword [esp]
+    add esp, 4
+
+
+    pop edi                   ; ripristina i registri da preservare
+    pop esi
+    pop ebx
+    pop ebp
+
+    ret
+
+
--- src/pst64c.c
+++ src/pst64c.c
@@ -40,6 +40,9 @@
 */
 
 #include <stdlib.h>
+
+#include <stdint.h>
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -53,17 +56,17 @@
 
 #define random() (((type) rand())/RAND_MAX)
 
-type hydrophobicity[] = {1.8, -1, 2.5, -3.5, -3.5, 2.8, -0.4, -3.2, 4.5, -1, -3.9, 3.8, 1.9, -3.5, -1, -1.6, -3.5, -4.5, -0.8, -0.7, -1, 4.2, -0.9, -1, -1.3, -1};		
-type volume[] = {88.6, -1, 108.5, 111.1, 138.4, 189.9, 60.1, 153.2, 166.7, -1, 168.6, 166.7, 162.9, 114.1, -1, 112.7, 143.8, 173.4, 89.0, 116.1, -1, 140.0, 227.8, -1, 193.6, -1};
-type charge[] = {0, -1, 0, -1, -1, 0, 0, 0.5, 0, -1, 1, 0, 0, 0, -1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, -1};
+type hydrophobicity[] = {1.8, -1, 2.5, -3.5, -3.5, 2.8, -0.4, -3.2, 4.5, -1, -3.9, 3.8, 1.9, -3.5, -1, -1.6, -3.5, -4.5, -0.8, -0.7, -1, 4.2, -0.9, -1, -1.3, -1};		// hydrophobicity
+type volume[] = {88.6, -1, 108.5, 111.1, 138.4, 189.9, 60.1, 153.2, 166.7, -1, 168.6, 166.7, 162.9, 114.1, -1, 112.7, 143.8, 173.4, 89.0, 116.1, -1, 140.0, 227.8, -1, 193.6, -1};		// volume
+type charge[] = {0, -1, 0, -1, -1, 0, 0, 0.5, 0, -1, 1, 0, 0, 0, -1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, -1};		// charge
 
 typedef struct {
 	char* seq;		// sequenza di amminoacidi
-	int N;		// lunghezza sequenza
-	unsigned int sd; 	// seed per la generazione	casuale
+	int N;			// lunghezza sequenza
+	unsigned int sd; 	// seed per la generazione casuale
 	type to;		// temperatura INIZIALE
 	type alpha;		// tasso di raffredamento
-	type k;			// costante
+	type k;		// costante
 	VECTOR hydrophobicity; // hydrophobicity
 	VECTOR volume;		// volume
 	VECTOR charge;		// charge
@@ -91,7 +94,7 @@ typedef struct {
 */
 
 void* get_block(int size, int elements) { 
-	return _mm_malloc(elements*size,16); 
+	return _mm_malloc(elements*size,32); 
 }
 
 void free_block(void* p) { 
@@ -100,6 +103,10 @@ void free_block(void* p) {
 
 MATRIX alloc_matrix(int rows, int cols) {
 	return (MATRIX) get_block(sizeof(type),rows*cols);
+}
+
+VECTOR alloc_vector(int n) {
+	return (VECTOR) get_block(sizeof(type),n);
 }
 
 int* alloc_int_matrix(int rows, int cols) {
@@ -113,6 +120,12 @@ char* alloc_char_matrix(int rows, int cols) {
 void dealloc_matrix(void* mat) {
 	free_block(mat);
 }
+
+void dealloc_vector(void* mat) {
+	free_block(mat);
+}
+
+
 
 /*
 * 
@@ -277,11 +290,324 @@ void gen_rnd_mat(VECTOR v, int N){
 
 // PROCEDURE ASSEMBLY
 extern void prova(params* input);
+extern MATRIX backbone_asm(int N, VECTOR phi, VECTOR psi);
+extern type distance_asm(int i, int j, MATRIX coords);
+
+// PROCEDURE IN C
+	//TRIGONOMETRICHE
+type cosine(type theta){
+	type t0 = 1;
+	type t1 = -theta*theta / 2;
+	type t2 = theta*theta*theta*theta / 24; //fact(4)
+	type t3 = -theta*theta*theta*theta*theta*theta / 720;  //fact(6)
+
+	return t0 + t1 + t2 + t3;
+}
+type sine(type theta){
+	type t0 = theta;
+	type t1 = -theta*theta*theta / 6; //fact(3)
+	type t2 = theta*theta*theta*theta*theta / 120; //fact(5)
+	type t3 = -theta*theta*theta*theta*theta*theta*theta / 5040; //fact(7)
+
+	return t0 + t1 + t2 + t3;
+}
+
+//funzione per la normalizzazione dell'axis
+void normalize_vector(VECTOR axis){
+	type magn = sqrt(axis[0]*axis[0] + axis[1]*axis[1] + axis[2]*axis[2]);
+	if(magn != 0){
+		axis[0] /= magn;
+		axis[1] /= magn;
+		axis[2] /= magn;
+	}
+}
+//ROTAZIONE
+MATRIX rotation(VECTOR axis, type theta){
+	MATRIX R = alloc_matrix(3,3);
+	normalize_vector(axis);
+	type scalar_prod = axis[0]*axis[0] + axis[1]*axis[1] + axis[2]*axis[2];
+	axis[0] = axis[0]/scalar_prod;
+	axis[1] = axis[1]/scalar_prod;
+	axis[2] = axis[2]/scalar_prod;
+
+
+	type a = cosine(theta/2);
+	type stheta = sine(theta/2);
+
+	type b = -1 * axis[0] * stheta;
+	type c = -1 * axis[1] * stheta;
+	type d = -1 * axis[2] * stheta;
+
+	R[0] = a*a + b*b - c*c - d*d;
+	R[1] = 2*b*c + 2*a*d;
+	R[2] = 2*b*d - 2*a*c;
+	
+	R[3] = 2*b*c - 2*a*d;
+	R[4] = a*a + c*c - b*b - d*d;
+	R[5] = 2*c*d + 2*a*b;
+
+	R[6] = 2*b*d + 2*a*c;
+	R[7] = 2*c*d - 2*a*b;
+	R[8] = a*a + d*d - b*b - c*c;
+	return R;
+}
+
+VECTOR matrix_product(VECTOR v, MATRIX m){
+	VECTOR res = alloc_vector(4);
+	res[0] = v[0]*m[0] + v[1]*m[3] + v[2]*m[6];
+	res[1] = v[0]*m[1] + v[1]*m[4] + v[2]*m[7];
+	res[2] = v[0]*m[2] + v[1]*m[5] + v[2]*m[8];
+	res[3] = 0;
+
+	return res;
+}
+
+type distance_angles(type phi, type psi, type a_phi, type a_psi){
+	return sqrt(pow(phi-a_phi, 2) + pow(psi-a_psi, 2));
+}
+
+type min(type a, type b){
+	if(a<b)
+		return a;
+	return b;
+}
+
+MATRIX backbone(int N, VECTOR phi, VECTOR psi){
+
+	MATRIX coords = alloc_matrix(N*3,4);
+
+
+	type dist0=1.46; //rcan
+	type dist1=1.52; //rcac
+	type dist2=1.33; //rcn
+	type angle_cnca = 2.124;
+
+	//Primo N
+	coords[0] = 0;
+	coords[1] = 0;
+	coords[2] = 0;
+	coords[3] = 0; //PADDING PER SSE
+
+	//Primo CA
+	coords[4] = dist0;
+	coords[5] = 0;
+	coords[6] = 0;
+	coords[7] = 0; //PADDING PER SSE
+
+	VECTOR tmp = alloc_vector(4);
+	for(int i = 0; i<N; i++){
+		int idx = i*3;
+		MATRIX R;
+		VECTOR neww;
+		if(i>0){
+
+			//N
+			tmp[0] = coords[4*(idx-1)] - coords[4*(idx-2)];
+			tmp[1] = coords[4*(idx-1)+1] - coords[4*(idx-2)+1];
+			tmp[2] = coords[4*(idx-1)+2] - coords[4*(idx-2)+2];
+			tmp[3] = 0;
+			R = rotation(tmp, angle_cnca);
+			tmp[0] = 0;
+			tmp[1]= dist2;
+			tmp[2]= 0;
+			tmp[3]= 0;
+			neww = matrix_product(tmp, R);
+			coords[idx*4]=coords[(idx-1)*4]+neww[0];
+			coords[idx*4+1]=coords[(idx-1)*4+1]+neww[1];
+			coords[idx*4+2]=coords[(idx-1)*4+2]+neww[2];
+			coords[idx*4+3]=0; //PADDING PER SSE
+
+			//CA
+			tmp[0] = coords[idx*4] - coords[4*(idx-1)];
+			tmp[1] = coords[idx*4+1] - coords[4*(idx-1)+1];
+			tmp[2] = coords[idx*4+2] - coords[4*(idx-1)+2];
+			tmp[3] = 0;
+			R = rotation(tmp, phi[i]);
+			tmp[0] = 0;
+			tmp[1]= dist0;
+			tmp[2]= 0;
+			tmp[3]= 0;
+			neww = matrix_product(tmp, R);
+			coords[4*(idx+1)]=coords[4*idx]+neww[0];
+			coords[4*(idx+1)+1]=coords[4*idx+1]+neww[1];
+			coords[4*(idx+1)+2]=coords[4*idx+2]+neww[2];
+			coords[4*(idx+1)+3]=0; //PADDING PER SSE
+		}	
+
+		//C
+		tmp[0] = coords[4*(idx+1)] - coords[4*(idx)];
+		tmp[1] = coords[4*(idx+1)+1] - coords[4*(idx)+1];
+		tmp[2] = coords[4*(idx+1)+2] - coords[4*(idx)+2];
+		tmp[3] = 0;
+		R = rotation(tmp, psi[i]);
+		tmp[0] = 0;
+		tmp[1]= dist1;
+		tmp[2]= 0;
+		tmp[3]= 0;
+		neww = matrix_product(tmp, R);
+		coords[4*(idx+2)]=coords[4*(idx+1)]+neww[0];
+		coords[4*(idx+2)+1]=coords[4*(idx+1)+1]+neww[1];
+		coords[4*(idx+2)+2]=coords[4*(idx+1)+2]+neww[2];
+		coords[4*(idx+2)+3]=0; 
+
+	}
+	dealloc_vector(tmp);
+	return coords;
+}
+
+MATRIX c_alpha_coords(MATRIX coords, int N){
+	MATRIX c_alpha_coords = alloc_matrix(N,4);
+	for(int i=0; i<N; i++){
+		int inx = i*12;
+		int idx = i*4;
+		c_alpha_coords[idx] = coords[inx+4];
+		c_alpha_coords[idx+1] = coords[inx+5];
+		c_alpha_coords[idx+2] = coords[inx+6];
+		c_alpha_coords[idx+3] = coords[inx+7];
+	}
+	
+	return c_alpha_coords;
+}
+
+type distance(int i, int j, MATRIX coords){
+	int real_i = i*4;
+	int real_j = j*4;
+	return sqrt(pow(coords[real_j]-coords[real_i], 2) +
+				pow(coords[real_j+1]-coords[real_i+1], 2) +
+				pow(coords[real_j+2]-coords[real_i+2], 2) +
+				pow(coords[real_j+3] -coords[real_i+3], 2)
+	);
+}
+
+//ENERGIE
+type rama_energy(VECTOR phi, VECTOR psi, int N){
+	type alpha_phi = -57.8;
+	type alpha_psi = -47.0;
+	type beta_phi = -119.0;
+	type beta_psi = 113.0;
+	type energy = 0;
+	for (int i = 0; i<N; i++){
+		type a_dist = distance_angles(phi[i], psi[i], alpha_phi, alpha_psi);
+		type b_dist = distance_angles(phi[i], psi[i], beta_phi, beta_psi);
+		energy = energy + 0.5*min(a_dist, b_dist);
+	}
+	return energy;
+}
+
+type hydrophobic_energy(char* s, MATRIX c_alpha_coords, int N){
+	type energy = 0;
+	for(int i = 0; i<N; i++){
+		for(int j = i+1; j<N; j++ ){
+			type dist = (type)distance_asm(i,j, c_alpha_coords);
+			if( dist < 10.0){
+				energy = energy + (hydrophobicity[s[i]-'A']*hydrophobicity[s[j]-'A'])/dist;
+			}
+		}
+	}
+	return energy;
+}
+
+type electrostatic_energy(char* s, MATRIX c_alpha_coords, int N){
+	type energy = 0;
+	for(int i = 0; i<N; i++){
+		for(int j = i+1; j<N; j++ ){
+			type dist = (type)distance_asm(i,j, c_alpha_coords);
+			if( dist < 10.0 && charge[s[i]-'A'] != 0 && charge[s[j]-'A'] != 0){
+				energy = energy + (charge[s[i]-'A']*charge[s[j]-'A'])/(dist*4);
+			}
+		}
+	}
+	return energy;
+}
+
+type packing_energy(char* s, MATRIX c_alpha_coords, int N){
+	type energy = 0;
+	for(int i = 0; i<N; i++){
+		type density = 0;
+		for(int j = 0; j<N; j++ ){
+			type dist = (type)distance_asm(i,j, c_alpha_coords);
+			if(i!=j && dist < 10.0){
+				type d = (volume[s[j]-'A']/(dist*dist*dist));
+				density = density + d;
+			}
+		}
+		type voldens = (volume[s[i]-'A']-density)*(volume[s[i]-'A']-density);
+		energy = energy + voldens;
+	}
+	return energy;
+}
+
+type energy(char* s, int N, VECTOR phi, VECTOR psi){
+
+	//MATRIX coords = backbone(N, phi, psi);
+	MATRIX coords = (MATRIX) backbone_asm(N, phi, psi);
+	MATRIX c_alpha = c_alpha_coords(coords, N);
+
+	type rama = rama_energy(phi, psi, N);
+	type hydrophobic = hydrophobic_energy(s, c_alpha, N) ;
+	type electrostatic = electrostatic_energy(s, c_alpha, N);
+	type packing = packing_energy(s, c_alpha, N);
+
+	type w_rama = 1.0;
+	type w_hydrophobic = 0.5;
+	type w_electrostatic = 0.2;
+	type w_packing = 0.3;
+
+	dealloc_matrix(coords);
+	dealloc_matrix(c_alpha);
+
+	return w_rama*rama + w_hydrophobic*hydrophobic + w_electrostatic*electrostatic + w_packing*packing;
+}
+
+void simulated_annealing(char* s, int N, VECTOR phi, VECTOR psi, type T0, type alpha, type k){
+	printf("Simulated Annealing start...");
+	type E = energy(s,N,phi,psi);
+	type T = T0;
+	int t = 0;
+	printf("SA: t > %i - ENERGIA> %f - DELTA-E > NA\n", t, E);
+
+	while (T>0){
+		int i = (int) (random()*N);
+		type dphi = (random()*2 * M_PI) - M_PI;
+		type dpsi = (random()*2 * M_PI) - M_PI;
+		phi[i]+= dphi;
+		psi[i]+= dpsi;
+
+
+		type new_energy = energy(s,N,phi,psi);
+		type deltaE = new_energy - E;
+		printf("SA: t > %i - ENERGIA> %d - DELTA-E > %d \n", t, E, deltaE);
+		if(deltaE<= 0){
+			printf("SA: t>%i - Accettazione nuova configurazione per indice %i [dE <= 0] \n", t,i);
+			E = new_energy;
+		}else{
+
+			type sp = -deltaE/(k*T);
+			type P = exp(sp);
+			type r = random();
+			if(r<= P){
+				printf("SA: t>%i - Accettazione nuova configurazione per indice %i [r <= P] \n", t,i);
+				E = energy(s,N,phi,psi);
+			}else{
+				printf("SA: t>%i -Rifiuto nuova configurazione per indice %i \n",t,i);
+				phi[i]-= dphi;
+				psi[i]-= dpsi;
+				
+			}
+			
+		}
+		printf("\n ------------------ \n");
+		t++;
+		T = T0 - sqrt(alpha*t);
+	}
+	
+	printf("SA: Energia = %f\n", E);
+
+	return;
+}
 
 void pst(params* input){
-	// --------------------------------------------------------------
-	// Codificare qui l'algoritmo di Predizione struttura terziaria
-	// --------------------------------------------------------------
+	simulated_annealing(input->seq, input->N, input->phi, input->psi, input->to,input->alpha, input->k);
 }
 
 int main(int argc, char** argv) {
@@ -304,9 +630,9 @@ int main(int argc, char** argv) {
 	input->sd = -1;		
 	input->phi = NULL;		
 	input->psi = NULL;
-	input->e = -1;
 	input->silent = 0;
 	input->display = 0;
+	input->e = -1;
 	input->hydrophobicity = hydrophobicity;
 	input->volume = volume;
 	input->charge = charge;
@@ -436,7 +762,7 @@ int main(int argc, char** argv) {
 	}
 
 	// COMMENTARE QUESTA RIGA!
-	prova(input);
+	//prova(input);
 	//
 
 	//
